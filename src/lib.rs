ec5de8efe512efe3d329f79358f918176460811d
jinser <cmdr.jv@gmail.com>
2023-03-16 01:06:40 +0800

    Update: select

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,42 @@
-use rusted_firebase::{Firebase, Paramable, Requestable};
+use http_req::request;
 use schedule_flows::schedule_cron_job;
-use serde_json::Value;
+use select::{document::Document, predicate::Class};
 use slack_flows::send_message_to_channel;
+
+const URI: &str = "https://news.ycombinator.com/front";
 
 #[no_mangle]
 pub fn run() {
     schedule_cron_job(
-        String::from("0 10 * * *"),
+        String::from("15 17 * * *"),
         String::from("cron_job_evoked"),
         callback,
     );
 }
 
 fn callback(_body: Vec<u8>) {
-    let firebase =
-        Firebase::new("https://hacker-news.firebaseio.com/v0/").expect("error init Firebase");
+    let mut writer = Vec::new();
+    let result = request::get(URI, &mut writer);
 
-    let cons = firebase
-        .at("topstories")
-        .limit_to_first(7)
-        .order_by("\"$key\"");
+    if let Ok(resp) = result {
+        if resp.status_code().is_success() {
+            let s = String::from_utf8(writer).unwrap();
+            let document = Document::from(s.as_str());
 
-    let resp = cons.get::<Vec<usize>>();
+            let msg = document
+                .find(Class("titleline"))
+                .zip(document.find(Class("hnuser")))
+                .filter_map(|(tl, user)| {
+                    tl.first_child().map(|node| {
+                        let title = node.text();
+                        let url = node.attr("href").unwrap_or_default();
+                        let author = user.text();
 
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build();
-
-    if let Ok(rt) = rt {
-        let item_ids = rt.block_on(resp).unwrap_or(vec![]);
-        let items = item_ids
-            .iter()
-            .map(|item_id| {
-                let cons = firebase.at("item").at(&item_id.to_string());
-                let resp = cons.get::<Value>();
-
-                let value = rt.block_on(resp).unwrap_or(Value::Null);
-
-                let by = value
-                    .get("by")
-                    .unwrap_or(&Value::Null)
-                    .as_str()
-                    .unwrap_or("Unknown `by`");
-                let title = value
-                    .get("title")
-                    .unwrap_or(&Value::Null)
-                    .as_str()
-                    .unwrap_or("Unknown `title`");
-                let url = value
-                    .get("url")
-                    .unwrap_or(&Value::Null)
-                    .as_str()
-                    .unwrap_or("Unknown `url`");
-
-                format!("#{}\n<{}|source> by {}", title, url, by)
-            })
-            .collect::<Vec<_>>()
-            .join("\n");
-
-        send_message_to_channel("ham-5b68442", "general", items);
+                        format!("- *{title}*\n<source|{url}> by {author}\n")
+                    })
+                })
+                .collect::<String>();
+            send_message_to_channel("ham-5b68442", "general", msg);
+        }
     }
 }
